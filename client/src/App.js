dcdfa8097d16e762de1b6e6971b6bb4b59deb773
Adam Risberg <myniztan@hotmail.com>
2019-11-23 02:21:23 -0600

    Add demo fetch to App

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,24 @@
 import React from "react";
 
 function App() {
+  const [text, setText] = React.useState("");
+
+  React.useEffect(() => {
+    fetch("/api/test")
+      .then(res => res.json())
+      .then(data => {
+        setText(data.text);
+      })
+      .catch(err => {
+        setText("Failed to fetch data from server.");
+        console.log(err);
+      });
+  });
+
   return (
-    <div>
+    <div style={{ textAlign: "center" }}>
       <h1>React Express Boilerplate</h1>
-      <p>Test</p>
+      <p>{text}</p>
     </div>
   );
 }
